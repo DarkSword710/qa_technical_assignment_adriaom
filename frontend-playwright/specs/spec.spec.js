f2466265963c910d4ac8306ca50ba3d51e92c9e2
Adrià Ortiz <AdriOrMa@gmail.com>
2025-07-21 19:19:57 +0200

    Initial Playwright test
    Initial tests on playwright to access a website and interact with it

--- frontend-playwright/specs/spec.spec.js
+++ frontend-playwright/specs/spec.spec.js
@@ -1,18 +1,27 @@
 const {test, expect} = require('@playwright/test');
 import specPage from '../page_objects/specPage';
 
-test.describe('Test Set name', () => {
+test.describe('QA Technical Assignment', () => {
+
+  //Need to find out how to ensure that I am logged out every iteration
   test.beforeAll(async () => {});
 
   test.afterEach(async ({page}) => {
 
   });
 
-  test('Scenario 1 name', async ({page}) => {
+  test('FRONTEND', async ({page}) => {
 
-  });
-
-  test('Scenario 2 name', async ({page}) => {
+    // Navigate to the page
+    await page.goto('https://www.saucedemo.com/');
+    //Fill in the username field (make variable for the fill data)
+    await page.locator('#user-name').fill('standard_user');
+    //Fill in the password field (need to make variable for the fill data)
+    await page.locator('#password').fill('secret_sauce');
+    //Click the log in button (could be filtered better)
+    await page.getByRole('button').click();
+    //await page.waitForURL('https://www.saucedemo.com/inventory.html');
+    //await page.getByRole('button').click();
 
   });
 
