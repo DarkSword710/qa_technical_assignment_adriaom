c46a0abbb7b2c6b82be0c6b33de152bd9b6589f3
Adrià Ortiz <AdriOrMa@gmail.com>
2025-07-25 13:20:28 +0200

    Checking URLs and accessing item page
    Code now awaits for the page URL expected when clicking a button and accesses the inventory item page (needs to be ammended to easily change the item to check)

--- frontend-playwright/specs/spec.spec.js
+++ frontend-playwright/specs/spec.spec.js
@@ -6,7 +6,7 @@ test.describe('QA Technical Assignment', () => {
   //Need to find out how to ensure that I am logged out every iteration
   test.beforeAll(async () => {});
 
-  test.afterEach(async ({page}) => {
+  test.beforeEach(async ({page}) => {
 
   });
 
@@ -14,14 +14,19 @@ test.describe('QA Technical Assignment', () => {
 
     // Navigate to the page
     await page.goto('https://www.saucedemo.com/');
-    //Fill in the username field (make variable for the fill data)
+    //Fill in the username field (need to make variable for the fill data)
     await page.locator('#user-name').fill('standard_user');
     //Fill in the password field (need to make variable for the fill data)
     await page.locator('#password').fill('secret_sauce');
     //Click the log in button (could be filtered better)
     await page.getByRole('button').click();
-    //await page.waitForURL('https://www.saucedemo.com/inventory.html');
-    //await page.getByRole('button').click();
+    //Waits for the inventory page to load
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+    //Items are NOT in ID order, since they get sorted alphabetically within the page. Second item (Bike Light) is ID 0
+    await page.locator('#item_0_title_link').click();
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory-item.html?id=0');
+    //await page.getByRole();
+
 
   });
 
